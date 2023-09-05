1fa76fbca79b17d77c9ee2f28bb25438f79c51a7
or-abdillh <hans.abdillh05@gmail.com>
2023-09-05 13:38:11 +0800

    Remove useless import

--- src/middlewares/atuhentication.middleware.ts
+++ src/middlewares/atuhentication.middleware.ts
@@ -1,4 +1,4 @@
-import { Response, Request, NextFunction, Application } from "express"
+import { Response, Request, NextFunction } from "express"
 import dotenv from "dotenv"
 import jwt from "jsonwebtoken"
 
